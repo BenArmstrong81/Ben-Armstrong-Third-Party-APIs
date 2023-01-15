9cf2c562c21a799a68ad97644f86e67680ef0068
Ben Armstrong <benarmstrong@live.com>
2023-01-15 23:38:58 +1030

    amended HTML, JavaScript and some manually overwritted CSS elements

--- assets/script.js
+++ assets/script.js
@@ -3,22 +3,15 @@
 // in the html.
 
 //---------------------------Variables used for JavaScript Functions:
-var timeDisplayEl = $('#date-display');
-var timePast
-var timePresent
-var timeFuture
-var timeBlockEntry = $('#description');
-// var hour9 = moment().hours(9).minutes(00).format('hh:mm A');
-// var hour10 = moment().hours(10).minutes(00).format('hh:mm A'); 
-// var hour11 = moment().hours(11).minutes(00).format('hh:mm A');
-// var hour12 = moment().hours(12).minutes(00).format('hh:mm A');  
-// var hour1 = moment().hours(1).minutes(00).format('hh:mm A');
-// var hour2 = moment().hours(2).minutes(00).format('hh:mm A');
-// var hour3 = moment().hours(3).minutes(00).format('hh:mm A');
-// var hour4 = moment().hours(4).minutes(00).format('hh:mm A');
-// var hour5 = moment().hours(5).minutes(00).format('hh:mm A');
-// var hour = [hour9, hour10, hour11, hour12, hour1, hour2, hour3, hour4, hour5];
+// var timeDisplayEl = $('#date-display');
+// var timePast = $('#textarea');
+// var timePresent = $('#textarea');
+// var timeFuture = $('#textarea');
+// var timeBlockEntry = $('#description');
 
+//---------------------------Variables used for JavaScript Functions:
+var timeDisplayEl = $('#date-display');
+var tableDisplayEl = $('#table-display')
 
 //---------------------------FUNCTION: for Displaying the date in header 
 function displayDate() {
@@ -26,82 +19,79 @@ function displayDate() {
   timeDisplayEl.text(rightNow);
 }
 
+//---------------------------Establish current date
+var dateStatement = moment().format('dddd, MMMM Do YYYY');
 
-function readScheduleFromStorage() {
-  var timeBlockEntry = localStorage.getItem('schedule');
-  if (schedule) {
-    schedule = JSON.parse(schedule);
-  } else { 
-      schedule = [];
+//---------------------------Establish current hour
+var currentHour = moment().hour()
+
+//---------------------------Establish an arry of hours > use to loop through  
+var hours = [];
+var availableHours = moment('12');
+new Array(24).fill().map((acc, index)=> {
+    hours.push(availableHours.format('hA'))
+    availableHours = availableHours.add(1, 'hour');
+})
+
+//---------------------------Loop through array of hours for 9 hours (available working hours in the day)
+//---------------------------Add class & styles to elements as they are created
+for(i = 0; i < 9; i++) {
+    // set tableRowEl id to the index displayed on on the calendar to easily identify which row belongs to which hour
+    tableRowEl = $('<tr>').attr('id', i+9).addClass('custom-row');
+    timeCol = $('<td>').text(hours[i+9]).addClass('col-2 col-md-1 hour text-center py-3')
+    activityCol = $('<td>').addClass('col-9').attr('class', 'note')
+    activityTextarea = $('<textarea>').addClass('w-100 description').attr('data-id', i+9);
+    saveCol = $('<td>').addClass('col-1 p-0')
+    // Adding an eventlistener on each button that will run saveNote()
+    saveBtn = $('<button>').addClass('saveBtn').on('click', saveNote);
+    saveIcon = $('<i>').addClass('fas fa-save');
+
+    // Add Past/Present/Future rules based on current index + 9 (the starting hour point)
+    if(currentHour < (i + 9)) {
+    activityCol.addClass('future')
+    } else if (currentHour === (i + 9)) {
+        activityCol.addClass('present')
+    } else if (currentHour > i + 9){
+        activityCol.addClass('past');
+    }
+
+//---------------------------Append created sections to the tableDisplayElement
+    saveBtn.append(saveIcon);   
+    activityCol.append(activityTextarea);
+    saveCol.append(saveBtn);
+    tableRowEl.append(
+        timeCol,
+        activityCol,
+        saveCol
+    );
+    tableDisplayEl.append(tableRowEl);
 }
-return schedule;
-}
-//---------------------------FUNCTION: Saves Aray of project and store in local storage
-function saveScheduleToStorage(schedule) {
-    localStorage.setItem('schedule', JSON.stringify(schedule));
-}
-
-//---------------------------FUNCTION: Retreives local storage and displays it
-function printScheduleData() {
-
-//----------clears current page
-    // timeBlockEntry.empty();
-
-//-----------Retrieves projects keyed in from loacal storage
-    var schedule = readScheduleFromStorage();
-
-//------------------LOOP: creates a row for each project input
-// for (var i = 0; i < hour.length; i += 1) {
-//     var schedule = schedule[i];
-//     var projectHour = dayjs(schedule.hour);
-
-//-----------gets date/time for start of today
-    var hour = dayjs().startOf('hour');
-
-//-----------Add class to row by comparing project date to today's date
-if (calendarTime.isBefore(hour)) {
-  timeBlockEntry.addClass('past');
-} else if (calendarTime.isSame(hour)) {
-  timeBlockEntry.addClass('present');
-} else (calendarTime.isAfter(hour)); {
-  timeBlockEntry.addClass('future');
+//---------------------------Function to save note into local storage
+function saveNote(event){
+    // Establish variable to hold the id of the parent row used to identify which hour/row the note belongs to
+    const parentId = $(this).parents('tr').attr('id');
+    const activity = $(this).parent().siblings('.note').children('textarea').val();
+    if (activity) {
+        localStorage.setItem(parentId, activity)
+    }
+    return;
 }
 
-//-----------add project to local storage
-var schedule = readScheduleFromStorage();
-schedule.push(newschedule);
-saveScheduleToStorage(schedule);
+//---------------------------function to loadNotes on page refresh
+function loadNotes(){
+    for(i=9; i<18; i++){
+       let storedNote = localStorage.getItem(i);
 
-//-----------print project data
-printProjectData();
-
+       if(storedNote){
+        let noteEl = $("textarea[data-id=" + i + "]");
+        noteEl.val(storedNote);
+       }
+    }
 }
+//---------------------------Function: To load stored notes on page load
+loadNotes();
 
-
-    
-
-// $(function () {
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-
-// });
-
-//---------------------------Running the timer updating every second
+//---------------------------Function: Running the Date within the Header element:
 displayDate();
+
+
